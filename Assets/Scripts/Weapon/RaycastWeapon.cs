79daa1f5a47d1b5b95c46e7cc756d87da15ef84b
keziaglr <keziagloria24@gmail.com>
2021-10-13 15:54:09 +0700

    bullet

--- Assets/Scripts/Weapon/RaycastWeapon.cs
+++ Assets/Scripts/Weapon/RaycastWeapon.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class RaycastWeapon : MonoBehaviour
 {
     public bool isFiring = false;
-    public ParticleSystem muzzleFlash;
+    public ParticleSystem[] muzzleFlash;
+    public ParticleSystem hitEffect;
+    public Transform raycastOrigin;
+    public Transform raycastDestination;
+
+    Ray ray;
+    RaycastHit hitInfo;
 
     public void StartFiring()
     {
@@ -14,7 +20,15 @@ public class RaycastWeapon : MonoBehaviour
         {
             particle.Emit(1);
         }
-        muzzleFlash.Emit(1);
+        ray.origin = raycastOrigin.position;
+        ray.direction = raycastOrigin.forward;
+        if(Physics.Raycast(ray, out hitInfo)){
+            Debug.Log(hitInfo.collider.gameObject.name);
+            //Debug.DrawLine(ray.origin, hitInfo.point, Color.red, 1.0f);
+            hitEffect.transform.position = hitInfo.point;
+            hitEffect.transform.forward = hitInfo.normal;
+            hitEffect.Emit(1);
+        }
     }
 
     public void StopFiring()
